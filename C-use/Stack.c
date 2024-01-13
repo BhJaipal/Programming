a1a3d5272cad2c8f8f2dfef0697dd9859fb610c7
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2024-01-13 20:48:01 +0530

    Update Stack.c

--- C-use/Stack.c
+++ C-use/Stack.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef char string[];
 typedef int Node;
 
-typedef struct Array {
+typedef struct Stack {
   Node *list;
   int length;
-} Array;
+} Stack;
 
-int len(Array);
+int len(Stack);
 /**
  * @param list takes an array of integers
  * @returns An array struct
  */
-Array createArr(int list[]) {
-  Array arr;
-  arr.length = 5;
+Stack createArr(int list[], int len) {
+  Stack arr;
+  arr.length = len;
   arr.list = malloc(5 * sizeof(Node));
-  for (int i = 0; i < 5; i++) {
+  for (int i = 0; i < len; i++) {
     arr.list[i] = list[i];
   }
   return arr;
@@ -28,8 +27,8 @@ Array createArr(int list[]) {
  * @param elem Element to be added to array
  * @return Nothing
  */
-void push(Array *arr, int elem) {
-  Array defref = *arr;
+void push(Stack *arr, int elem) {
+  Stack defref = *arr;
   arr->length++;
   Node arr2[sizeof(defref) / sizeof(int)];
   for (int i = 0; i < len(defref); i++) {
@@ -38,8 +37,8 @@ void push(Array *arr, int elem) {
   arr->list = realloc(arr->list, sizeof(elem) + (sizeof(int) * len(defref)));
   arr->list[arr->length - 1] = elem;
 }
-int pop(Array *arr) {
-  Array arr2 = *arr;
+int pop(Stack *arr) {
+  Stack arr2 = *arr;
   Node last = arr2.list[len(arr2) - 1];
   arr->list[arr2.length - 1] = 0;
   arr->length--;
@@ -47,10 +46,10 @@ int pop(Array *arr) {
   return last;
 }
 
-int len(Array arr) { return arr.length; }
+int len(Stack arr) { return arr.length; }
 
-int printArray(Array *arr) {
-  Array arr2 = *arr;
+int printArray(Stack *arr) {
+  Stack arr2 = *arr;
   for (int i = 0; i < len(arr2); i++) {
     printf("%d ", arr->list[i]);
   }
@@ -59,9 +58,8 @@ int printArray(Array *arr) {
 }
 
 int main() {
-  string name = "Jaipal";
   Node list[5] = {3, 5, 4, 2, 7};
-  Array arr = createArr(list);
+  Stack arr = createArr(list, 5);
   printf("Array is: ");
   printf("Length of array: %d\n", printArray(&arr));
   printf("Poped last element: %d\n", pop(&arr));
