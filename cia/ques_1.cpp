07ee599e14288057a7151633a92d8c42a00e0991
NITIN YADAV <58939117+nitinya9av@users.noreply.github.com>
2024-04-22 11:26:07 +0530

    Class_Hierarchy_added
    This Question demands Class Hierarchy.

--- cia/ques_1.cpp
+++ cia/ques_1.cpp
@@ -1,83 +1,102 @@
+//Design a class hierarchy to represent different shapes such as Circle, Rectangle, and 
+//Triangle. Include methods to calculate the area and perimeter of each shape. Implement 
+//appropriate constructors and destructor. 
+
 #include <iostream>
 #include <cmath>
+using namespace std;
 
-class Circle
+class Shape
 {
-	int radius;
-
-public:
-	Circle(int r)
-	{
-		radius = r;
-	}
-	float perimeter()
-	{
-		return 2 * 3.1415 * radius;
-	}
-	float area()
-	{
-		return radius * radius * 3.1415;
-	}
-	~Circle()
-	{
-		std::cout << "Circle Destructor called" << std::endl;
-	}
+    public:
+    virtual void area() = 0;
+    virtual void perimeter() = 0;
+    ~Shape()
+    {
+        cout<<"Destructor of Shape"<<endl;
+    }
 };
-class Rectangle
+
+class Circle: public Shape
 {
-	int length, breadth;
-
-public:
-	Rectangle(int l, int b)
-	{
-		length = l;
-		breadth = b;
-	}
-	float perimeter()
-	{
-		return 2 * (length + breadth);
-	}
-	float area()
-	{
-		return length * breadth;
-	}
-	~Rectangle()
-	{
-		std::cout << "Rectangle Destructor called" << std::endl;
-	}
+    float radius;
+    public:
+    Circle(float r)
+    {
+        radius = r;
+    }
+    void area()
+    {
+        cout<<"Area of Circle: "<<3.14*radius*radius<<endl;
+    }
+    void perimeter()
+    {
+        cout<<"Perimeter of Circle: "<<2*3.14*radius<<endl;
+    }
+    ~Circle()
+    {
+        cout<<"Circle Destroyed"<<endl;
+    }
 };
-class Triangle
+
+class Rectangle: public Shape
 {
-	int a, b, c;
-
-public:
-	Triangle(int s1, int s2, int s3) : a(s1), b(s2), c(s3)
-	{
-	}
-	float perimeter()
-	{
-		return a + b + c;
-	}
-	float area()
-	{
-		float s = perimeter() / 2;
-		return std::sqrt(s * (s - a) * (s - b) * (s - c));
-	}
-	~Triangle()
-	{
-		std::cout << "Triangle Destructor called" << std::endl;
-	}
+    float length, breadth;
+    public:
+    Rectangle(float l, float b)
+    {
+        length = l;
+        breadth = b;
+    }
+    void area()
+    {
+        cout<<"Area of Rectangle: "<<length*breadth<<endl;
+    }
+    void perimeter()
+    {
+        cout<<"Perimeter of Rectangle: "<<2*(length+breadth)<<endl;
+    }
+    ~Rectangle()
+    {
+        cout<<"Rectangle Destroyed"<<endl;
+    }
 };
+
+class Triangle: public Shape
+{
+    float a, b, c;
+    public:
+    Triangle(float x, float y, float z)
+    {
+        a = x;
+        b = y;
+        c = z;
+    }
+    void area()
+    {
+        float s = (a+b+c)/2;
+        cout<<"Area of Triangle: "<<sqrt(s*(s-a)*(s-b)*(s-c))<<endl;
+    }
+    void perimeter()
+    {
+        cout<<"Perimeter of Triangle: "<<a+b+c<<endl;
+    }
+    ~Triangle()
+    {
+        cout<<"Triangle Destroyed"<<endl;
+    }
+};
+
 int main()
 {
-	Rectangle r(10, 5);
-	std::cout << "Rectangle Area: " << r.area() << "\n";
-	std::cout << "Rectangle perimeter: " << r.perimeter() << "\n\n";
-	Circle c(7);
-	std::cout << "Circle Area: " << c.area() << "\n";
-	std::cout << "Circle perimeter: " << c.perimeter() << "\n\n";
-	Triangle tri(3, 4, 5);
-	std::cout << "Triangle Area: " << tri.area() << "\n";
-	std::cout << "Triangle perimeter: " << tri.perimeter() << "\n\n";
-	return 0;
+    Circle c(5);
+    c.area();
+    c.perimeter();
+    Rectangle r(5, 10);
+    r.area();
+    r.perimeter();
+    Triangle t(3, 4, 5);
+    t.area();
+    t.perimeter();
+    return 0;
 }
