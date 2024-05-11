223df961827ab22e8ceb015f4b767ee7a4658a67
BhJaipa <105307427+BhJaipal@users.noreply.github.com>
2024-05-11 13:43:34 +0530

    26 and 27

---
+++ sem2-practical/ques-26.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+
+class Shape {
+protected:
+	Shape() = default;
+
+public:
+	friend Shape createShape();
+	virtual float area() { return 0; };
+	virtual ~Shape() { cout << "Shape Destructor\n"; };
+	virtual void findArea(int sides);
+};
+
+class Rectangle : public Shape {
+	float l, b;
+
+public:
+	Rectangle(float _l, float _b) : l(_l), b(_b) {}
+	float area() { return l * b; }
+	~Rectangle() { cout << "Rectangle Destructor\n"; }
+	void findArea(int sides) {}
+};
+class Circle : public Shape {
+	float r;
+
+public:
+	Circle(float _r) : r(_r) {}
+	float area() { return 3.14 * r * r; }
+	~Circle() { cout << "Circle Destructor\n"; }
+	void findArea(int sides) {}
+};
+
+void Shape::findArea(int sides) {
+	Shape *shape;
+	if (sides == 4) {
+		cout << "Rectangle\n";
+		cout << "Enter length and breadth: ";
+		int l, b;
+		cin >> l >> b;
+		shape = new Rectangle(l, b);
+	}
+	else if (sides == 1) {
+		cout << "Circle\n";
+		cout << "Enter Radius: ";
+		int r;
+		cin >> r;
+		shape = new Circle(r);
+	}
+	if (sides != 4 && sides != 1) { cout << "Invalid Input\n"; }
+	else {
+		float a = shape->area();
+		cout << "Area: " << a << endl;
+		delete shape;
+	}
+}
+Shape createShape() { return Shape(); }
+int main() {
+	Shape s = createShape();
+	s.findArea(0);
+	cout << endl;
+	s.findArea(1);
+	cout << endl;
+	return 0;
+}
+/*
+Output:
+Invalid Input
+
+Circle
+Enter Radius: 7
+Area: 153.86
+Circle Destructor
+Shape Destructor
+
+Shape Destructor
+*/
---
+++ sem2-practical/ques-27.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Student {
+protected:
+	string name, profession;
+	int age;
+
+public:
+	virtual void input() = 0;
+	virtual void display() = 0;
+};
+class Engineering : public Student {
+public:
+	void input() {
+		cout << "Enter name: ";
+		cin >> name;
+		cout << "Enter age: ";
+		cin >> age;
+		cout << "Enter profession(Mechanical, Civil, Electrical): ";
+		cin >> profession;
+	}
+	void display() {
+		cout << "Name: " << name << endl;
+		cout << "Age: " << age << endl;
+		cout << "Profession: " << profession << endl;
+	}
+};
+class Medical : public Student {
+public:
+	void input() {
+		cout << "Enter name: ";
+		cin >> name;
+		cout << "Enter age: ";
+		cin >> age;
+		cout << "Enter profession(Nurse, Doctor, Dentist): ";
+		cin >> profession;
+	}
+	void display()
+	{
+		cout << "Name: " << name << endl;
+		cout << "Age: " << age << endl;
+		cout << "Profession: " << profession << endl;
+	}
+};
+class Science : public Student {
+public:
+	void input() {
+		cout << "Enter name: ";
+		cin >> name;
+		cout << "Enter age: ";
+		cin >> age;
+		cout << "Enter profession(Biologist, Chemist, Physician): ";
+		cin >> profession;
+	}
+	void display() {
+		cout << "Name: " << name << endl;
+		cout << "Age: " << age << endl;
+		cout << "Profession: " << profession << endl;
+	}
+};
+int main() {
+	Student *s[3];
+	s[0] = new Engineering;
+	s[0]->input();
+	cout << "\n";
+	s[1] = new Medical;
+	s[1]->input();
+	cout << "\n";
+	s[2] = new Science;
+	s[2]->input();
+	cout << "\n\n";
+	for (int i = 0; i < 3; i++) {
+		s[i]->display();
+		cout << "\n";
+	}
+	return 0;
+}
+/*
+Output:
+Enter name: Name1
+Enter age: Age1
+Enter profession(Mechanical, Civil, Electrical): Electrical
+
+Enter name: Name2
+Enter age: Age2
+Enter profession(Nurse, Doctor, Dentist): Dentist
+
+Enter name: Name3
+Enter age: Age3
+Enter profession(Biologist, Chemist, Physician): Physician
+
+
+Name: Name1
+Age: Age1
+Profession: Electrical
+
+Name: Name2
+Age: Age2
+Profession: Dentist
+
+Name: Name3
+Age: Age3
+Profession: Physician
+*/
