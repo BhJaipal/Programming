14ef6b101479789cc8a9f21561600b9b2419121a
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2024-05-12 15:24:12 +0530

    Update ques-12.cpp

--- sem2-practical/ques-12.cpp
+++ sem2-practical/ques-12.cpp
@@ -4,19 +4,17 @@ using namespace std;
 class B;
 class A {
 	int x;
-
 public:
 	A(int a = 0) : x(a) {}
 	friend int sum(A &, B &);
 };
+
 class B {
 	int y;
-
 public:
 	B(int b = 0) : y(b) {}
 	friend int sum(A &, B &);
 };
-
 int sum(A &a, B &b) { return a.x + b.y; }
 
 int main() {
