e31ee964e927d413ddc744851e5b01d44e94d0ae
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2024-05-12 18:07:31 +0530

    Update ques-17.cpp

--- sem2-practical/ques-17.cpp
+++ sem2-practical/ques-17.cpp
@@ -5,7 +5,6 @@ class Time {
 	int hr, min;
 
 public:
-	Time(int h, int m) : hr(h), min(m) {}
 	Time(int mins) : hr(mins / 60), min(mins % 60) {}
 	void display() { cout << (hr > 9 ? "" : "0") << hr << ":" << (min > 9 ? "" : "0") << min << endl; }
 };
