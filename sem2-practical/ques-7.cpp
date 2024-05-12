8eb3add390473f49935604011e124039d767535a
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2024-05-12 13:48:40 +0530

    Update ques-7.cpp

--- sem2-practical/ques-7.cpp
+++ sem2-practical/ques-7.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 using namespace std;
 
-class Complex
-{
+class Complex {
 	int real, imag;
 
 public:
@@ -11,8 +10,7 @@ public:
 	Complex operator+(Complex &obj) { return Complex(real + obj.real, imag + obj.imag); }
 	void display() { cout << real << (imag < 0 ? "" : "+") << imag << "i" << endl; }
 };
-int main()
-{
+int main() {
 	Complex c1(10, 5), c2(2, 4);
 	Complex c3 = c1 + c2;
 	c3.display();
