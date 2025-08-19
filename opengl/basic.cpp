212469217d57ff604ba6280211c9365e2d8d0598
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2025-08-19 09:03:01 +0530

    Triangle go brrrr

--- opengl/basic.cpp
+++ opengl/basic.cpp
@@ -1,7 +1,10 @@
 #include <GL/glew.h>
 #include <GL/gl.h>
 #include <fstream>
+#include <glm/ext/matrix_clip_space.hpp>
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/transform.hpp>
 #include <math.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
@@ -12,49 +15,91 @@ static void glfw_error_callback(int error, const char *description) {
 	std::cerr << "Glfw Error " << error << description << "\n";
 }
 GLuint LoadShaders(const char * vertex_file_path,const char * fragment_file_path);
+
 int main() {
+	glewExperimental = true;
 	glfwSetErrorCallback(glfw_error_callback);
 	if (!glfwInit())
 		return 1;
 
+	glfwWindowHint(GLFW_SAMPLES, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-	GLFWwindow *window = glfwCreateWindow(1280, 720, "New Opengl", NULL, NULL);
+	GLFWwindow *window = glfwCreateWindow(950, 1000, "New Opengl", NULL, NULL);
 	if (window == NULL)
 		return 1;
+
+
 	glfwMakeContextCurrent(window);
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Failed to initialize GLEW\n");
+		return -1;
+	}
 	// Enable vsync
 	glfwSwapInterval(1);
 
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
-	// Dark blue background
-	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
 
 	GLuint VertexArrayID;
 	glGenVertexArrays(1, &VertexArrayID);
 	glBindVertexArray(VertexArrayID);
 
-	GLuint programID = LoadShaders( "vertex.glsl", "fragment.glsl" );
-	
-	static const GLfloat g_vertex_buffer_data[] = { 
-		-1.0f, -1.0f, 0.0f,
-		 1.0f, -1.0f, 0.0f,
-		 0.0f,  1.0f, 0.0f,
+	static const GLfloat g_vertex_buffer_data[] = {
+		-1.f, -1.0f, 0.0f,
+		1.0f, -1.0f, 0.0f,
+		0.0f,  1.0f, 0.0f,
 	};
-
+	// This will identify our vertex buffer
 	GLuint vertexbuffer;
+	// Generate 1 buffer, put the resulting identifier in vertexbuffer
 	glGenBuffers(1, &vertexbuffer);
+	// The following commands will talk about our 'vertexbuffer' buffer
 	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	// Give our vertices to OpenGL.
 	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
+	GLuint programID = LoadShaders( "./vertex.glsl", "./fragment.glsl" );
+
+	glm::mat4 myMatrix{
+		6.f, 7, 2, 8,
+		2, 8, 4, 3,
+		5, 1, 9, 8,
+		2, 4, 3, 6};
+	glm::vec4 myVector{6, 9, 4, 3};
+	// fill myMatrix and myVector somehow
+	glm::vec4 transformedVector = myMatrix * myVector;
+	
+	// Projection matrix: 45° Field of View, 4:3 ratio, display range: 0.1 unit <-> 100 units
+	glm::mat4 Projection = glm::perspective(
+			glm::radians(45.0f),
+			(float) 400 / (float) 600, // width / height
+			.1f, 100.0f);
+
+	// Camera matrix
+	glm::mat4 View = glm::lookAt(
+		glm::vec3(4,1,3), // Camera is at (2,1,3), in World Space
+		glm::vec3(0,0,0), // and looks at the origin
+		glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
+    );
+	// Model matrix: an identity matrix (model will be at the origin)
+	glm::mat4 Model = glm::mat4(1.0f);
+	// Our ModelViewProjection: multiplication of our 3 matrices
+	glm::mat4 mvp = Projection * View * Model; // Remember, matrix multiplication is the other way around
+
+	// blue background
+	glClearColor(0.7f, 0.4f, 1.f, 0.0f);
+
 	while (!glfwWindowShouldClose(window)) {
-		glfwPollEvents();
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);;
+
+
 		glUseProgram(programID);
+		GLuint MatrixID = glGetUniformLocation(programID, "MVP");
+		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &mvp[0][0]);
 
 		glEnableVertexAttribArray(0);
 		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
@@ -66,11 +111,12 @@ int main() {
 			0,                  // stride
 			(void*)0            // array buffer offset
 		);
-		// Draw the triangle !
 		glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
 		glDisableVertexAttribArray(0);
 
+
         glfwSwapBuffers(window);
+		glfwPollEvents();
 	}
 	glfwDestroyWindow(window);
 	glfwTerminate();
@@ -91,7 +137,7 @@ GLuint LoadShaders(const char * vertex_file_path,const char * fragment_file_path
 		sstr << VertexShaderStream.rdbuf();
 		VertexShaderCode = sstr.str();
 		VertexShaderStream.close();
-	}else{
+	} else {
 		printf("Impossible to open %s. Are you in the right directory ? Don't forget to read the FAQ !\n", vertex_file_path);
 		getchar();
 		return 0;
@@ -111,7 +157,7 @@ GLuint LoadShaders(const char * vertex_file_path,const char * fragment_file_path
 	int InfoLogLength;
 
 	// Compile Vertex Shader
-	printf("Compiling shader : %s\n", vertex_file_path);
+	printf("Compiling shader  %s\n", vertex_file_path);
 	char const * VertexSourcePointer = VertexShaderCode.c_str();
 	glShaderSource(VertexShaderID, 1, &VertexSourcePointer , NULL);
 	glCompileShader(VertexShaderID);
@@ -126,7 +172,7 @@ GLuint LoadShaders(const char * vertex_file_path,const char * fragment_file_path
 	}
 
 	// Compile Fragment Shader
-	printf("Compiling shader : %s\n", fragment_file_path);
+	printf("Compiling shader: %s\n", fragment_file_path);
 	char const * FragmentSourcePointer = FragmentShaderCode.c_str();
 	glShaderSource(FragmentShaderID, 1, &FragmentSourcePointer , NULL);
 	glCompileShader(FragmentShaderID);
--- opengl/fragment.glsl
+++ opengl/fragment.glsl
@@ -1,5 +1,6 @@
 #version 330 core
 out vec3 color;
-void main(){
-  color = vec3(1,0,0);
+
+void main() {
+	color = vec3(0.75,0.9,1);
 }
