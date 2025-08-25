45f722b5766ef8a94d802df76234e7451ca62640
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2025-08-25 20:53:22 +0530

    Remove useless files

--- asm/c-impl.h
+++ asm/c-impl.h
@@ -9,7 +9,7 @@ extern void munmap();
 extern void putchar(char c);
 extern int pow(int b, int e);
 extern void write_uint(unsigned int x);
-extern unsigned int read_uint();
+extern unsigned read_uint();
 extern void write_int(int x);
 extern int read_int();
 
