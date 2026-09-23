280bf95f979b6e840c446b9fb5ec044b5ff9b1b1
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2026-09-23 09:53:07 +0530

    Modify vertex shader to include color output

--- opengl/vertex.glsl
+++ opengl/vertex.glsl
@@ -1,8 +1,10 @@
 #version 330 core
-layout(location = 0) in vec3 vertexPosition_modelspace;
+in vec3 vertex;
+in vec3 color;
 uniform mat4 MVP;
+out vec3 fragColor;
 
-void main(){
-  gl_Position = MVP * vec4(vertexPosition_modelspace, 1);
+void main() {
+  gl_Position = MVP * vec4(vertex, 1);
+  fragColor = color;
 }
-
